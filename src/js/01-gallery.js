d2359d7d26ba83e512a3df40fdb6c3f2819a523e
xSaizo <mihai.andrey96@yahoo.com>
2023-10-16 09:21:03 -0700

    gh-pages

--- src/js/01-gallery.js
+++
@@ -1,7 +0,0 @@
-// Add imports above this line
-import { galleryItems } from './gallery-items';
-// Change code below this line
-import SimpleLightbox from "simplelightbox";
-import "simplelightbox/dist/simple-lightbox.min.css";
-import { listEl } from './lightboxgallery'
-console.log(galleryItems);
